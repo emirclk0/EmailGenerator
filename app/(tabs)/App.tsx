b384ccb26b43a28e58e4d4299627bdf713b460a7
Emirhan Çelik <emirhan01adana@hotmail.com>
2024-08-05 20:17:07 +0300

    Redirects fixed.

--- app/(tabs)/App.tsx
+++ app/(tabs)/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import Login from './login';
-import Generator from './generator';
+import Login from './Login';
+import Generator from './Generator';
 
 export type RootStackParamList = {
   Login: undefined;
--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -15,7 +15,7 @@ export default function TabLayout() {
         headerShown: false,
       }}>
       <Tabs.Screen
-        name="login"
+        name="Login"
         options={{
           title: 'Login',
           tabBarIcon: ({ color, focused }) => (
@@ -24,7 +24,7 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="generator"
+        name="Generator"
         options={{
           title: 'Generator',
           tabBarIcon: ({ color, focused }) => (
